cfa13d5e7c26472dec9f9bdcc1e862fb481efbbf
4ntho0ny <anthonyhildebrandchendrik@gmail.com>
2024-12-06 14:32:18 -0400

    Tirando o teste de conexão  do arquivo index.html

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-    require 'modelo/databaseConnection.php';
-    $connection = new DatabaseConnection();
-    $connection->databaseConnection();
-    die();
     header("location: ./layout/login.php");
 ?>
