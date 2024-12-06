2b439f92a480a1fae0394ff24c3ddd7bc81d7774
4ntho0ny <anthonyhildebrandchendrik@gmail.com>
2024-12-06 08:15:40 -0400

    Refactor databaseConnnection.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php 
+<?php
+    require 'modelo/databaseConnection.php';
+    $connection = new DatabaseConnection();
+    $connection->databaseConnection();
+    die();
     header("location: ./layout/login.php");
 ?>
---
+++ modelo/databaseConnection.php
@@ -0,0 +1,32 @@
+<?php
+    class DatabaseConnection {        
+        private const DATABASE = "Jogos";
+        private const DSN = "mysql:host=localhost;dbname="; //DataBase Server Name
+        private const USER = "root";
+        private const PASSWORD = "Anthony@761613";
+        
+        function getConstants() {
+            $reflectionClass = new ReflectionClass($this);
+            return $reflectionClass->getConstants();
+        }
+
+        function databaseConnection() {
+            $constants = $this->getConstants();
+            $dsn = $constants["DSN"];
+            $user = $constants["USER"];
+            $db = $constants["DATABASE"];
+            $password = $constants["PASSWORD"];
+
+            $pdo = null;
+            try {
+                // PDO Connection
+                $pdo = new PDO($dsn.$db, $user, $password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "It's work!";
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            }
+            return $pdo;
+        }
+    }
+?>
